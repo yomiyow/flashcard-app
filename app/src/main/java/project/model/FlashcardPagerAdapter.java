59ae96387029771706bbf4f3f2406ffb5e630f3d
Romeo Quiñones <romeoquinones4@gmail.com>
2024-12-16 16:12:21 +0800

    Add flipping functionality when a flashcard click

--- app/src/main/java/project/model/FlashcardPagerAdapter.java
+++ app/src/main/java/project/model/FlashcardPagerAdapter.java
@@ -1,5 +1,9 @@
 package project.model;
 
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -29,10 +33,38 @@ public class FlashcardPagerAdapter extends RecyclerView.Adapter<FlashcardPagerAd
         return new FlashcardViewHolder(flashcardItem);
     }
 
+    private void flipCard(View front, View back) {
+        AnimatorSet flipOut = (AnimatorSet) AnimatorInflater.loadAnimator(context, R.animator.flip_out);
+        AnimatorSet flipIn = (AnimatorSet) AnimatorInflater.loadAnimator(context, R.animator.flip_in);
+
+        flipOut.setTarget(front);
+        flipIn.setTarget(back);
+
+        flipOut.start();
+        flipIn.start();
+
+        flipOut.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                front.setVisibility(View.GONE);
+                back.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
     @Override
     public void onBindViewHolder(@NonNull FlashcardPagerAdapter.FlashcardViewHolder holder, int position) {
         FlashcardModel.TermDefinition termDefinition = termDefinitionList.get(position);
         holder.termTV.setText(termDefinition.getTerm());
+        holder.definitionTV.setText(termDefinition.getDefinition());
+
+        holder.itemView.setOnClickListener((v) -> {
+            if (holder.termTV.getVisibility() == View.VISIBLE) {
+                flipCard(holder.termTV, holder.definitionTV);
+            } else {
+                flipCard(holder.definitionTV, holder.termTV);
+            }
+        });
     }
 
     @Override
@@ -43,10 +75,12 @@ public class FlashcardPagerAdapter extends RecyclerView.Adapter<FlashcardPagerAd
     static class FlashcardViewHolder extends RecyclerView.ViewHolder {
 
         TextView termTV;
+        TextView definitionTV;
 
         public FlashcardViewHolder(@NonNull View itemView) {
             super(itemView);
             termTV = itemView.findViewById(R.id.term);
+            definitionTV = itemView.findViewById(R.id.definition);
         }
     }
 }
